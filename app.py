2111afffc64f6e041b5677d674a5b37d40e58f20
HARDIK SOMKUWAR <120791564+AlMIGHTY-HARDIK@users.noreply.github.com>
2025-03-25 16:49:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -776,4 +776,4 @@ server.mount("/", WSGIMiddleware(app.server))
 import os
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
